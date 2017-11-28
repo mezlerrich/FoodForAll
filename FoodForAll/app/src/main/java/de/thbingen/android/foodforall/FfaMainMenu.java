bdee59c4244dbcba76ba58b2d15c2327011fc7c2
richard <richard.mezler@th-bingen.de>
2017-11-28 10:40:54 +0100

    First initialize Android Project

---
+++ FoodForAll/app/src/main/java/de/thbingen/android/foodforall/FfaMainMenu.java
@@ -0,0 +1,13 @@
+package de.thbingen.android.foodforall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FfaMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ffa_main_menu);
+    }
+}
