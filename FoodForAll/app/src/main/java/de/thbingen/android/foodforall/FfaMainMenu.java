e18271dee43d82a46e82cf83089f745002161dbe
richard <richard.mezler@th-bingen.de>
2017-11-28 10:58:07 +0100

    userInterface: create MainMenu

--- FoodForAll/app/src/main/java/de/thbingen/android/foodforall/FfaMainMenu.java
+++ FoodForAll/app/src/main/java/de/thbingen/android/foodforall/FfaMainMenu.java
@@ -2,12 +2,34 @@ package de.thbingen.android.foodforall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class FfaMainMenu extends AppCompatActivity {
-
+public class FfaMainMenu extends AppCompatActivity
+{
+    /**
+     * An array of buttons, to get them from the main menu.
+     */
+    private Button[] _mainMenuBtns;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ffa_main_menu);
+
+        initializeButtons();
+    }
+
+    /**
+     * Initialize the buttons from the main menu.
+     */
+    private void initializeButtons()
+    {
+        _mainMenuBtns = new Button[] {
+                (Button)findViewById(R.id.sharedBtn),
+                (Button)findViewById(R.id.mapBtn),
+                (Button)findViewById(R.id.carteBtn),
+                (Button)findViewById(R.id.messageBtn)
+        };
+
     }
 }
